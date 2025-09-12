c9f1b029c54bfdf4a67ab9ef03d2529c12777481
Phillip van Staden <prvanstaden.phillip@gmail.com>
2025-09-12 15:49:07 +0200

    webhooks

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,29 @@
-// Manage databse connection effeciently
-//Server actions (connectToDatabase() creates new db connection elke keer sonder hierdie(Gebuik selfde connection))
+// lib/database/index.ts
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
-//let cached
-const cached = (global as any).mongoose || { conn: null, promise: null }; //If no mongoose cache create empty one
-//chached hold chaced connection to db
-export const connectToDatabase = async () => {
-  //
-  if (cached.conn) return cached.connect; //Is cached already conntected(running for first time)
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing"); //Connect to cach or create new connection
+const globalAny: any = global as any;
+const cached = globalAny.mongoose || {
+  conn: null as any,
+  promise: null as any,
+};
+
+if (!globalAny.mongoose) {
+  globalAny.mongoose = cached;
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn; // <-- return the existing connection
+
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
+
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "Evently",
+      dbName: "evently",
       bufferCommands: false,
     });
 
