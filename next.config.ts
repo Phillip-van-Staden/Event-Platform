201bed1a2b87e681510f0198d4435ca533127f64
Phillip van Staden <prvanstaden.phillip@gmail.com>
2025-09-16 08:05:59 +0200

    Event details

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ufs.sh",
+        port: "",
+      },
+    ],
+  },
   /* config options here */
 };
 
